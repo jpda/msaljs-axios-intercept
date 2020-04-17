99c46fb3bf637f74f7d2ad39e8e6ceb7113dd718
jpda <john.dandison@live.com>
2020-04-17 16:34:13 -0400

    adding b2c config

--- src/msal/MsalConfig.ts
+++ src/msal/MsalConfig.ts
@@ -1,10 +1,20 @@
 const MsalConfig = {
     config: {
+        // aad configuration
+        // auth: {
+        //     clientId: "31c0ca04-16fb-49b6-83a2-e8c8487ea4fd",
+        //     authority: "https://login.microsoftonline.com/98a34a88-7940-40e8-af71-913452037f31/v2.0",
+        //     redirectUri: "https://msaljs-axios.jpda.app/auth",
+        //     navigateToLoginRequestUrl: false
+        // },
+
+        // b2c configuration
         auth: {
-            clientId: "31c0ca04-16fb-49b6-83a2-e8c8487ea4fd",
-            authority: "https://login.microsoftonline.com/98a34a88-7940-40e8-af71-913452037f31",
+            clientId: "5db7bd2a-1c59-40de-b638-2bd0b07a358d",
+            authority: "https://jpdab2c.b2clogin.com/jpdab2c.onmicrosoft.com/B2C_1_susi_v2",
             redirectUri: "https://msaljs-axios.jpda.app/auth",
-            navigateToLoginRequestUrl: false
+            navigateToLoginRequestUrl: false,
+            validateAuthority: false
         },
         cache: {
             cacheLocation: "sessionStorage" // session storage is more secure, but prevents single-sign-on from working. other option is 'localStorage'
@@ -12,7 +22,11 @@ const MsalConfig = {
     },
     // this is marked as the default, as the scopes for individual pages may be different
     defaultRequestConfiguration: {
-        scopes: ["api://msaljs.jpda.app/power", "api://msaljs.jpda.app/wake"],
+        // aad scopes
+        // scopes: ["api://msaljs.jpda.app/power", "api://msaljs.jpda.app/wake"],
+
+        // b2c api scopes
+        scopes: ["https://jpdab2c.onmicrosoft.com/msaljs-axios-api/stuff.read", "https://jpdab2c.onmicrosoft.com/msaljs-axios-api/otherstuff.execute"]
     }
 }
 export default MsalConfig;
